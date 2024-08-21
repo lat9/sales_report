6258df54467016fa842ea830e9dd87ee7874fb5f
lat9 <lat9@vinosdefrutastropicales.com>
2024-08-21 18:45:11 -0400

    Updating plugin directory for v4.0.2.

--- zc_plugins/SalesReport/v4.0.2/manifest.php
+++ zc_plugins/SalesReport/v4.0.2/manifest.php
@@ -1,11 +1,11 @@
 <?php
 return [
-    'pluginVersion' => 'v4.0.0',
+    'pluginVersion' => 'v4.0.2',
     'pluginName' => 'Sales Report II',
     'pluginDescription' => 'Generate filtered reports of your site\'s sales.',
     'pluginAuthor' => 'Vinos de Frutas Tropicales (lat9)',
     'pluginId' => 9, // ID from Zen Cart forum
-    'zcVersions' => ['v158', 'v158a'],
+    'zcVersions' => [],
     'changelog' => '', // online URL (eg github release tag page, or changelog file there) or local filename only, ie: changelog.txt (in same dir as this manifest file)
     'github_repo' => 'https://github.com/lat9/sales_report', // url
     'pluginGroups' => [],
