298e55b4d1a1e4ace34b627bde027022ec3e0eda
pRose on charmes <prose@rossroberts.com>
2024-08-21 15:18:37 -0700

    deregister old sales report

--- zc_plugins/SalesReport/v4.0.1/Installer/ScriptedInstaller.php
+++ zc_plugins/SalesReport/v4.0.1/Installer/ScriptedInstaller.php
@@ -5,7 +5,10 @@ class ScriptedInstaller extends ScriptedInstallBase
 {
     protected function executeInstall()
     {
-        zen_deregister_admin_pages(['statsSalesReport']);
+        zen_deregister_admin_pages([
+                                       'statsSalesReport',
+                                       'stats_sales_report',
+                                   ]);
         zen_register_admin_page(
             'statsSalesReport', 'BOX_REPORTS_SALES_REPORT2', 'FILENAME_STATS_SALES_REPORT2', '', 'reports', 'Y');
     }
